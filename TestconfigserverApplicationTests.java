469f39c70b7664192128700d1deea6333fda13e2
sridhar94sri <105697500+sridhar94sri@users.noreply.github.com>
2022-05-17 11:09:03 +0530

    test

---
+++ TestconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mphasis.testconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
